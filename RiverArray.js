6a3f4f9514dc507f9bad00d7b9773df307ebf4c9
RiversOffline <42424091+RiversOffline@users.noreply.github.com>
2018-08-17 15:54:10 -0400

    Add files via upload

--- RiverArray.js
+++ RiverArray.js
@@ -1,9 +1,43 @@
-RiverArray =[{"Name":"Snake River","Section":"Flagg Ranch Gorge","Difficulty":"Novice","Quality":"9","Writeup":"Snake River Writeup:
+RiverArray =[{"Name":"Snake River","Section":"Pacific Landing","Difficulty":"Beginner","Quality":"10","Writeup":"Snake River Writeup:
 
-Snake River, Yellowstone Park [South Entrance picnic area] to Flagg Ranch parking [2.7 mi.] \r\r\rFlagg Ranch gorge, Cl 2-, very nice  \rShuttle on US 89. Usually up [Flagg Ranch =300CFS]. \r\r\rContinue further to Lizzard Creek C.G. [addl. 10 mi. Cl 1]. \r\r\rTeton Park fee + launch permit + WYO permit; \r\r\rBeware of Flagg Ranch section in high water.","WriteupLength":371},{"Name":"Snake River","Section":"Deadmans","Difficulty":"Beginner","Quality":"10","Writeup":"Snake River Writeup:
+Snake River, Pacific Landing to Deadmans Bar, 11 mi., Cl 1+, scenic, Teton entry fee PLUS $10 boat fee, US89 shuttle
 
-Snake River, Deadmans Bar to Moose Vis. Ctr., 10 mi., Cl 1+, $10 boat fee, scenic (Tetons), usually up, shuttle on 89\r\r\rThe best way to see the Tetons!  The river has strong current - so you don’t have to paddle much.  You do need some boat control to avoid strainers.","WriteupLength":296},{"Name":"Snake River","Section":"Jackson Lake","Difficulty":"Beginner","Quality":"9","Writeup":"Snake River Writeup:
 
-Snake River, Jackson Lake outflow to Raft P.I, 4.5 mi., very scenic, Coyotes, Pelicans, Beaver, Teton Park entry fee PLUS boat fee, poke around in the Oxbow bend, shuttle on US 89 & Teton Park Rd. [Moran=?CFS]","WriteupLength":237},{"Name":"Snake River","Section":"Pacific Landing","Difficulty":"Beginner","Quality":"10","Writeup":"Snake River Writeup:
+The best way to see the Tetons!  The river has strong current - so you don’t have to paddle much.  You do need some boat control to avoid strainers.","WriteupLength":304},
 
-Snake River, Pacific Landing to Deadmans Bar, 11 mi., Cl 1+, scenic, Teton entry fee PLUS $10 boat fee, US89 shuttle\r\r\rThe best way to see the Tetons!  The river has strong current - so you don’t have to paddle much.  You do need some boat control to avoid strainers.","WriteupLength":295}]
+
+
+
+{"Name":"Snake River","Section":"Deadmans","Difficulty":"Beginner","Quality":"10","Writeup":"Snake River Writeup:
+
+Snake River, Deadmans Bar to Moose Vis. Ctr., 10 mi., Cl 1+, $10 boat fee, scenic (Tetons), usually up, shuttle on 89
+
+
+The best way to see the Tetons!  The river has strong current - so you don’t have to paddle much.  You do need some boat control to avoid strainers.","WriteupLength":305},
+
+
+
+
+{"Name":"Snake River","Section":"Flagg Ranch Gorge","Difficulty":"Novice","Quality":"9","Writeup":"Snake River Writeup:
+
+Snake River, Yellowstone Park [South Entrance picnic area] to Flagg Ranch parking [2.7 mi.] 
+
+
+Flagg Ranch gorge, Cl 2-, very nice  
+Shuttle on US 89. Usually up [Flagg Ranch =300CFS]. 
+
+
+Continue further to Lizzard Creek C.G. [addl. 10 mi. Cl 1]. 
+
+
+Teton Park fee + launch permit + WYO permit; 
+
+
+Beware of Flagg Ranch section in high water.","WriteupLength":410},
+
+
+
+
+{"Name":"Snake River","Section":"Jackson Lake","Difficulty":"Beginner","Quality":"9","Writeup":"Snake River Writeup:
+
+Snake River, Jackson Lake outflow to Raft P.I, 4.5 mi., very scenic, Coyotes, Pelicans, Beaver, Teton Park entry fee PLUS boat fee, poke around in the Oxbow bend, shuttle on US 89 & Teton Park Rd. [Moran=?CFS]","WriteupLength":237}]
