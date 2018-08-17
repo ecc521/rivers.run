2302d3a24a039ade974f5c8e01f0ca5e0fa9ed68
RiversOffline <42424091+RiversOffline@users.noreply.github.com>
2018-08-17 12:32:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,10 @@ if (window.innerWidth<560 && window.innerHeight>window.innerWidth) {
     GetId("Rivers").style.setProperty('--textsize', '12px')
 
 }
-window.addEventListener("orientationchange", setTimeout(function() {
+window.addEventListener("orientationchange", setTimeout(RotateHandler, 100))
+
+                        
+function RotateHandler() {
 if (window.innerWidth<560 && window.innerHeight>window.innerWidth) {
     if (window.innerWidth < 300) {
     GetId("RotateMobile").innerHTML = "<span style=\"background-color: orange\"><strong>Rotating your display to Landscape mode is recommended.</strong></span>"
@@ -60,7 +63,9 @@ else {
     alert(window.innerWidth + " " + window.innerHeight)
     GetId("Rivers").style.setProperty('--textsize', '18px')
 }
-}), 100);
+});
+
+
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('https://riversoffline.github.io/serviceworker.js')
