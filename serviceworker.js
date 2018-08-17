bf74844f4179b52450f1417d3e3f5c6a4d5ccfda
RiversOffline <42424091+RiversOffline@users.noreply.github.com>
2018-08-16 21:42:18 -0400

    Fix updating errors

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,24 @@
 self.addEventListener('activate', function(event) {
 caches.delete('RiversOffline');
 });
+
 self.addEventListener('fetch', function(event) {
+    var Request = fetch(event.request)
+    
+    Request.then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+    });
+  
+    Request.catch(function() {
+      return caches.match(event.request);
+    })
+                               
+                               
+  );
+});
+
+/*self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open('RiversOffline').then(function(cache) {
       return cache.match(event.request).then(function (response) {
@@ -12,4 +29,4 @@ self.addEventListener('fetch', function(event) {
       });
     })
   );
-});
+});*/
