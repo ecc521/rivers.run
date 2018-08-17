6c7cecf479f78118855d2f3c96fee7f85fcb8ced
RiversOffline <42424091+RiversOffline@users.noreply.github.com>
2018-08-16 21:44:46 -0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -15,7 +15,7 @@ self.addEventListener('fetch', function(event) {
     })
                                
                                
-  );
+  
 });
 
 /*self.addEventListener('fetch', function(event) {
