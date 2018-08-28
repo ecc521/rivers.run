9b2c187e5cf53eae50c50da76af61d21596fb574
RiversOffline <42424091+RiversOffline@users.noreply.github.com>
2018-08-28 16:09:39 -0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('RiversOffline').then(function(cache) {
+    caches.open('rivers.run').then(function(cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
