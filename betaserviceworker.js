53a4be4da4ecf7fdd02ebfe05503a2d7b68c8b35
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-09-07 16:51:33 -0400

    Create betaserviceworker.js

---
+++ betaserviceworker.js
@@ -0,0 +1,12 @@
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('rivers.run').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
