f40689d9cbae4c488cc9c9643d67be658db8f982
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-09-07 16:53:28 -0400

    Update betaserviceworker.js

--- betaserviceworker.js
+++ betaserviceworker.js
@@ -1,5 +1,6 @@
 self.addEventListener('fetch', function(event) {
   event.respondWith(
+    console.log(event.request)
     caches.open('rivers.run').then(function(cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function(response) {
