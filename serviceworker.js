206dbfd30aadffd64f41f10cc6d9c382f11eb400
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-09-09 18:40:17 -0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,4 @@
+<script>
 self.addEventListener('fetch', function(event) {  
 //Only cache rivers.run resources - don't cache other resources such as USGS gage data. That will uselessly fill up cache with dynamic URL's.
 if (event.request.url.indexOf("https://rivers.run") === 0) {
@@ -12,5 +13,16 @@ if (event.request.url.indexOf("https://rivers.run") === 0) {
     })
   );
 }
-else {event.respondWith(fetch(event.request))/*Fetch from network - AND DO NOT CACHE*/}  
-});
+else {
+event.respondWith(
+  /*USGS cache will need to be deleted on every page reload*/
+    caches.open('USGS').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+}  
