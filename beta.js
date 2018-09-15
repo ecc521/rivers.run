ada01937fa04a037f81606a0dbe2d6957392fcab
ecc521 <admin@rivers.run>
2018-09-15 19:14:51 -0400

    Update beta.js

--- beta.js
+++ beta.js
@@ -1,1 +1,25 @@
+function GetId(Name) {
+  return document.getElementById(Name)
+}
 
+function River(name, section, difficulty, quality, length, writeup, usgs, idbase) {
+  this.name = name
+  this.section = section
+  this.difficulty = difficulty
+  this.quality = quality
+  this.length = length
+  this.writeup = writeup
+  this.usgs = usgs
+  this.base = "b" + idbase
+  
+  this.display = function () {
+    var button = document.createElement("button")
+    button.className = this.base + 1
+    
+    
+  }
+  this.hide = function () {
+    
+  }
+
+}
