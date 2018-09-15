7e7deb4ec801e71b8041ae85fea2dc98ac6b3a23
ecc521 <admin@rivers.run>
2018-09-15 10:58:06 -0400

    Update betaserviceworker.js

--- betaserviceworker.js
+++ betaserviceworker.js
@@ -1,11 +1,11 @@
 //Currently returns protocol, subdomain, domain name, tld, and query. I've have a better version to put in soon.
 function URL(a){var b={};a=a.trim(),b.url=a,b.query=a.slice(a.indexOf("?")+1),b.query===a&&(b.query=void 0),0===a.indexOf("https://")?(b.protocol="https://",a=a.slice(8)):0===a.indexOf("http://")?(b.protocol="http://",a=a.slice(7)):b.protocol=void 0;var c=Math.min(a.indexOf("/"),a.indexOf("?"));return-1===c&&(c=Math.max(a.indexOf("/"),a.indexOf("?"))),c=-1===c?a:a.slice(0,c),b.domain=c,b.tld=c.slice(c.lastIndexOf(".")),c=c.slice(0,c.lastIndexOf(".")),b.rootdomain=c.slice(c.lastIndexOf(".")+1)+b.tld,b.subdomain=c.slice(0,c.lastIndexOf(".")+1),b}
-
+    
+    
 var List = {}
 List.url = []
 List.values = []
 var Age = Date.now()
-
 
 self.addEventListener('fetch', function(event) {
 if (URL(event.request.url).rootdomain === "rivers.run" || URL(event.request.url).domain === "ecc521.github.io") {
@@ -54,13 +54,4 @@ else {
     }
     
     return List.values[List.url.indexOf(event.request.url)].clone()
-}
-    
-    
-
-}
-  
-}()))
-}
-})
-
+}}()))}})
