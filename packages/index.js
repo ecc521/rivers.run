478d4bf8f8ec982a68e3dcb365199d73db13730e
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-03-19 19:13:07 -0400

    Refractoring

--- packages/index.js
+++ packages/index.js
@@ -735,61 +735,65 @@ module.exports.addLine = function (GraphName, timeframe, Source, canvas, horizon
 /* 3 */
 /***/ (function(module, exports) {
 
+//Auxillary Function
+//Sets the direction that the triangle on a header is facing
+function settri(elem, facingUpwards) {
+    let upArrow = "⬆"
+    let downArrow = "⬇"
+    let lastChar = elem.innerHTML.slice(-1)
+
+    //Make sure there is a triangle first.
+    if (lastChar === upArrow || lastChar === downArrow) {
+        elem.innerHTML = elem.innerHTML.slice(0,-1)
+    }
+    if (facingUpwards) {
+        elem.innerHTML += upArrow
+    }
+    else {
+        elem.innerHTML += downArrow
+    }
+}
+
+
 function TopBar() {
     this.create = function() {
         let button = document.createElement("button")
         button.id = "topbar"
         button.className = "riverbutton"
-      
+
         //Auxillary Function
         function NewSpan(Text) {
-          let span = document.createElement("span")
-          span.className = "riverspan"
-          span.innerHTML = Text
-          return span
+            let span = document.createElement("span")
+            span.className = "riverspan"
+            span.innerHTML = Text
+            return span
         }
-        
-        //Auxillary Function
-        function settri(elem, facingUpwards) {
-            let upArrow = "⬆"
-            let downArrow = "⬇"
-            let lastChar = elem.innerHTML.slice(-1)
-            
-          //Make sure there is a triangle first.
-          if (lastChar === upArrow || lastChar === downArrow) {
-           elem.innerHTML = elem.innerHTML.slice(0,-1)
-          }
-          if (facingUpwards) {
-            elem.innerHTML += upArrow
-          }
-          else {
-            elem.innerHTML += downArrow
-          }
-        }
-      
-      
+
+
+
+
         let span = NewSpan("River")
         settri(span, true)
 
         span.onclick = function() {
-          if (this.value) {
-            settri(this, false)
-            NewList("alphabetical", "sort", true)
-            this.value = 0
-          }
-          else {
-            settri(this, true)
-            NewList("alphabetical", "sort")
-            this.value = 1
-          }
+            if (this.value) {
+                settri(this, false)
+                NewList("alphabetical", "sort", true)
+                this.value = 0
+            }
+            else {
+                settri(this, true)
+                NewList("alphabetical", "sort")
+                this.value = 1
+            }
         }
         span.value = 1//Starts sorted alphabetically, a-z. The first sort needs to flip that.
         button.appendChild(span)
-        
-        
+
+
         button.appendChild(NewSpan("Section"))
 
-        
+
         span = NewSpan("Skill")
         settri(span, true)
         span.onclick = function() {
@@ -806,30 +810,30 @@ function TopBar() {
         }
         span.value = 0
         button.appendChild(span) 
-        
+
         span = NewSpan("Rating")
         settri(span, true)
 
         span.onclick = function() {
-                if (this.value === 1) {
-                    NewList("rating", "sort", true)
-                    settri(this, true)
-                    this.value = 0
-                }
-                else {
-                    NewList("rating", "sort")
-                    settri(this, false)
-                    this.value = 1
-                }
+            if (this.value === 1) {
+                NewList("rating", "sort", true)
+                settri(this, true)
+                this.value = 0
+            }
+            else {
+                NewList("rating", "sort")
+                settri(this, false)
+                this.value = 1
+            }
         }
         span.value = 0
         button.appendChild(span) 
-        
+
         button.appendChild(NewSpan("Flow Info"))
 
         return button
     }
-    
+
     this.delete = function() {
         let Node = GetId("topbar")
         if (Node) {
