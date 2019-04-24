968137ea1a37aab5e352cada6f14147118ccb131
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-04-24 15:07:59 -0400

    Create collectVisitorInformation.js

---
+++ collectVisitorInformation.js
@@ -0,0 +1,9 @@
+
+let data = {
+  userAgent: navigator.userAgent
+}
+
+let formURL = "https://docs.google.com/forms/d/e/1FAIpQLSfyTHeq0fp6-iofQacxfXcsSGjYymLJbLOPmlBBKBtttWvtcA/formResponse?ifq&entry.1198287313=" + JSON.stringify(data) + "&submit=Submit"
+
+
+fetch(formURL, {mode: "no-cors"})
