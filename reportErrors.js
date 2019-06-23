420ca5ef7574df9813e80d074d882bf1fa1b3830
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-06-22 22:54:09 -0700

    Improve data reporting

--- reportErrors.js
+++ reportErrors.js
@@ -2,10 +2,17 @@ let loadURL = require("./sendOffData.js").loadURL
 let userInfo = require("./collectVisitorInformation.js").data
 
 function reportError(...error) {
+
+	for (let i=0;i<error.length;i++) {
+		//Make sure some non-enumerable properties are included.
+		error[i] = JSON.parse(JSON.stringify(error[i], ["message", "arguments", "type", "name"]))
+	}
+
 	let data = {
 		userInfo,
 		error
 	}
+
 	let formURL = "https://docs.google.com/forms/d/e/1FAIpQLSd9h20lDBD4kTaNH8Ko6CAG9RGH1VCWwJKdfID_5RTTywTkFQ/formResponse?ifq&entry.1198287313=" + JSON.stringify(data) + "&submit=Submit"
 	formURL = encodeURI(formURL)
 
