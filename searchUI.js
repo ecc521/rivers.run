85958f1829d37c3fc19b4a8ce51d3ad3685545ee
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-07-21 18:25:59 -0400

    Fix and greatly simplify code to inform user of why obtaining location failed.

--- searchUI.js
+++ searchUI.js
@@ -87,34 +87,28 @@ async function calculateCoordinates() {
 			navigator.geolocation.getCurrentPosition(resolve, reject)
 		});
 	}
-	catch(e) {
+	catch(error) {
 		let output = "Your device encountered an error when attempting to find your position." //Message for POSITION_UNAVAILABLE error.
-		new Promise(() => {
+		if (error.PERMISSION_DENIED) {
 			//If the error is actually permission denied, check to see if we have location permission.
-			if (output.PERMISSION_DENIED) {
-				navigator.permissions.query({name:'geolocation'}).then((status) => {
-					if (status.state === "granted") {
-						//If we do have location permission, odds are that the browser did not. Tell that to the user.
-						output = "It appears that your browser could not access your location. Make sure that location services is enabled and allowed for your browser."
-					}
-					else if (status.state === "denied"){
-						//If the user denied permission, tell the user that they need to enable it.
-						output = "You denied rivers.run access to your location. Please enable location permission in site settings."
-					}
-					else if (status.state === "prompt") {
-						//If the user dismissed the prompt, tell them that they need to click Allow.
-						output = "It appears that you dismissed the permission prompt. To find your location, you need to grant the location permission."
-					}
-					resolve()
-				})
+			let status = await navigator.permissions.query({name:'geolocation'})
+			if (status.state === "granted") {
+				//If we do have location permission, odds are that the browser did not. Tell that to the user.
+				output = "It appears that your browser could not access your location. Make sure that location services is enabled and allowed for your browser."
 			}
-			else {resolve()} //The error was not permission denied. Continue.
-		}).then(() => {
-			output = "\nError message: " + e.message
-			alert(output)
-			clearInterval(progress)
-			status.innerHTML = output
-		})
+			else if (status.state === "denied"){
+				//If the user denied permission, tell the user that they need to enable it.
+				output = "You denied rivers.run access to your location. Please enable location permission in site settings."
+			}
+			else if (status.state === "prompt") {
+				//If the user dismissed the prompt, tell them that they need to click Allow.
+				output = "It appears that you dismissed the permission prompt. To find your location, you need to grant the location permission."
+			}
+		}
+		output = "\nError message: " + error.message
+		alert(output)
+		clearInterval(progress)
+		status.innerHTML = output
 	}
 
 
