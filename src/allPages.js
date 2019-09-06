21c9542e1d2d15894c859a9b85ecf95d0d948fe2
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-09-06 16:50:40 -0400

    Add legal stuff to bottom of pages in allPages.js

--- src/allPages.js
+++ src/allPages.js
@@ -283,12 +283,11 @@ catch (e) {
 
 try {
 	//Make sure I don't hate the font
-	styleSheet.insertRule("html body {font-family: Arial, Helvetica, sans-serif}", styleSheet.cssRules.length)
+	styleSheet.insertRule("html, body {font-family: Arial, Helvetica, sans-serif}", styleSheet.cssRules.length)
 }
 catch (e) {
 	console.error(e)
 }
-
 
 
 //Create navigation bar
@@ -377,6 +376,53 @@ try {
 catch (e) {
 	console.error(e)
 }
+
+
+
+
+
+//Add the disclaimer and other legal stuff at the bottom.
+try {
+	let div = document.createElement("div")
+	
+	div.style.textAlign = "center"
+	div.style.margin = "3em"
+	
+	let content = document.createElement("p")
+	div.appendChild(content)
+	
+	content.innerHTML = "Copyright © 2019, All Rights Reserved. "
+	
+	let privacy = document.createElement("a")
+	privacy.innerHTML = "Privacy Policy"
+	privacy.target = "_blank"
+	privacy.href = root + "legal/" + "Privacy Policy.html"
+	content.appendChild(privacy)
+	
+	content.innerHTML += " "
+	
+	let terms = document.createElement("a")
+	terms.innerHTML = "Terms of Service"
+	terms.target = "_blank"
+	terms.href = root + "legal/" + "Terms of Service.html"
+	content.appendChild(terms)
+	
+	content.innerHTML += " "
+	
+	let disclaimer = document.createElement("a")
+	disclaimer.innerHTML = "Disclaimer"
+	disclaimer.target = "_blank"
+	disclaimer.href = root + "legal/" + "DISCLAIMER.html"
+	content.appendChild(disclaimer)
+		
+	document.documentElement.appendChild(div)
+}
+catch (e) {
+	console.error(e)
+}
+
+
+
 
 
 //The remaining code is for the river-overview tags
