589c496bebc2f3f403812626e67882d6c59e71b8
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-10-05 14:01:54 -0400

    Update gauges.js
    Log more timing data while loading gauges.

--- server/gauges.js
+++ server/gauges.js
@@ -29,10 +29,10 @@ let timeToRequest = 1000*86400 //Milliseconds of time to request
 
 
 async function _loadFromUSGS(siteCodes) {
-	
+
 	//TODO: Calls should be batched up. I believe that USGS has a url length limit of 4096 characters.
 	//Probably use about 100-200 rivers per call due to performance reasons. When using 400, performance was almost 4 times worse.
-	
+
 	let startDT = "&startDT=" + new Date(Date.now()-timeToRequest).toISOString()
 	let endDT = "&endDT=" + new Date().toISOString() //endDT is optional. Will default to current time. USGS gauge prediction may be used if date in the future.
     let url = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=" + siteCodes.join(",") +  startDT  + endDT + "&parameterCd=00060,00065,00010,00011,00045&siteStatus=all"
@@ -71,7 +71,7 @@ async function loadFromUSGS(siteCodes) {
 			let buf = Buffer.from(jsonShrinker.stringify(newGauges[code]))
 
 			diskWrites.push(fs.promises.writeFile(filePath, buf).catch((e) => {console.error(filePath, e)}))
-			
+
 			diskWrites.push(compressor.brotliCompressAsync(buf, 7).then((compressedBuf) => {
 				//Compress at level 7. Its very good and much quicker than 11. Plus, we are talking like 600 bytes compressed here...
 				fs.promises.writeFile(filePath + ".br", compressedBuf).catch((e) => {console.error(filePath, e)})
@@ -81,9 +81,9 @@ async function loadFromUSGS(siteCodes) {
 	}
 
 
-async function loadData(siteCodes) {	
+async function loadData(siteCodes) {
 	let gauges = {}
-	
+
 	if (virtualGauges) {
 		try {
 			siteCodes = siteCodes.concat(await virtualGauges.getRequiredGauges())
@@ -93,44 +93,49 @@ async function loadData(siteCodes) {
 
 	//Filter out duplicate site names.
 	siteCodes = [...new Set(siteCodes)];
-	
+
 	//TODO: Look for ways to minimize the burden on USGS. This is currently somewhat expensive on them, probably running ~10 MB per 15 minutes.
 	//We may want to dynamically load more detailed information, or to simply use less except where requested for virtual gauges, etc.
-	
+
 	//Batch calls. I believe that USGS has a url length limit of 4096 characters, but they clearly have one (7000 cha/racters failed).
-	//Using 150 rivers/call. When using 400, performance was almost 4 times worse than 100-200 rivers/call.	
-	
+	//Using 150 rivers/call. When using 400, performance was almost 4 times worse than 100-200 rivers/call.
+
 	//Don't ask USGS for non-USGS gauges.
 	let usgsSites = siteCodes.filter((usgsID) => {
 		return usgsID.length > 7 && usgsID.length < 16 && !isNaN(Number(usgsID))
 	})
-	
+
 	let start = 0
 	let sitesPerBatch = 150 //150 sites at once
-	
+
 	let currentWrites = [];
-	
+
 	//TODO: Allow loading in paralell.
 	while (start < usgsSites.length) {
 		let end = start + sitesPerBatch
 		let arr = usgsSites.slice(start,end)
 		console.log("Loading sites " + start + " through " + end + " of " + usgsSites.length + ".")
 		let newGauges = await loadFromUSGS(arr)
-		
+
 		//Lets try to avoid too many open files at once. Wait for previous writes to finish.
 		console.time("Waiting on currentWrites")
 		await Promise.allSettled(currentWrites)
 		console.timeEnd("Waiting on currentWrites")
-		
+
+		console.time("Begin New Writes")
 		currentWrites = writeBatchToDisk(newGauges)
-		
+		console.timeEnd("Begin New Writes")
+
+		console.time("Shrink newGauges")
 		newGauges = shrinkUSGS.shrinkUSGS(newGauges) //Shrink newGauges.
+		console.timeEnd("Shrink newGauges")
+
 		Object.assign(gauges, newGauges) //Objects are references (so gauges object is modified)
 		start = end
 	}
-	
+
 	await Promise.allSettled(currentWrites)
-	
+
 	//Question: Should virtualGauges be added as gauges to rivers.run? They would need to be added to riverarray if so.
 	if (virtualGauges) {
 		console.log("Computing virtual gauges...")
@@ -143,7 +148,7 @@ async function loadData(siteCodes) {
 		catch (e) {console.log(e)}
 		console.log("Virtual gauges computed...")
 	}
-	
+
 	gauges.generatedAt = Date.now()
 	return gauges
 }
