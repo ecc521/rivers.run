b2c3c4d164b1858b7f82a0a61e99bbf17eaa6ad1
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-11-17 10:36:27 -0500

    Provide two examples for Amazon Alexa

--- server/assistantRequest.js
+++ server/assistantRequest.js
@@ -149,7 +149,7 @@ async function handleAlexaRequest(req, res) {
 				if (query.request.intent === undefined) {
 					queryResult = {
 						ssml: "<speak>Welcome to Rivers.run Water Levels! I can provide flow information on thousands of rivers across the country - just try \"Cheat Narrows Level\".</speak>",
-						text: "I can provide information on thousands of rivers across the country - just try \"Cheat Narrows Level\".",
+						text: "I can provide information on thousands of rivers across the country - just try \"Lower Gauley Level\" or \"is the Cheat Narrows running\".",
 						responseName: "Welcome to Rivers.run Water Levels!"
 					}
 					continueConversation = true
