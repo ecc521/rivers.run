c3fceacd87c6752a6b597ecf972cc89b39d5603f
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-06-15 19:49:32 -0400

    Move add all to favorites code - Fix bug where favoritesWidget could crash map

--- src/index.js
+++ src/index.js
@@ -175,6 +175,35 @@ function setSearchForLink() {
 	}
 }())
 
+const {loadFavorites, addRiverToFavorites, addRiversToFavorites} = require("./addToFavorites.js")
+
+//Enable the addAllToFavorites button.
+let addAllToFavorites = document.getElementById("addAllToFavorites")
+addAllToFavorites.addEventListener("click", function() {
+	let riversToAdd = window.currentlyShown //Set by NewList.js whenever this is set.
+
+	let message = `Add `
+	let riverAmount = riversToAdd.riverAmount
+	let gaugeAmount = riversToAdd.gaugeAmount
+
+	if (riverAmount === 1) {message += "1 river"}
+	else if (riverAmount > 1) {message += riverAmount + " rivers"}
+
+	if (riverAmount > 0 && gaugeAmount > 0) {message += " and "}
+
+	if (gaugeAmount === 1) {message += "1 gauge"}
+	else if (gaugeAmount > 1) {message += gaugeAmount + " gauges"}
+
+	if (riverAmount === 0 && gaugeAmount === 0) {return alert("Your search returned 0 results, so nothing can be added. ")}
+
+	if (confirm(message + " to favorites?")) {
+		addRiversToFavorites(riversToAdd)
+
+		window.location = "favorites.html"
+	}
+})
+
+
 //TODO: Delete this eventually. Add once release goes out.
 if (window?.Capacitor && window?.Capacitor?.getPlatform() === "ios") {
     try {
