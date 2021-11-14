af8c61d0f402f640b61c3e000662c1c5e92a849c
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-11-14 12:43:28 -0500

    Update site-setup.sh

--- site-setup.sh
+++ site-setup.sh
@@ -10,7 +10,7 @@ sudo git checkout express #TODO: Remove
 echo "Is the server already set up for multiple sites? If No, setup will be performed. "
 select yn in "Yes" "No"; do
     case $yn in
-        No ) ./server-setup.sh; break;;
+        No ) bash server-setup.sh; break;;
         Yes ) break;;
     esac
 done
