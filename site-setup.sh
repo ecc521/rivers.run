726bd3a773580c0234474f4cf2962ef202878e29
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-11-14 12:41:17 -0500

    Update site-setup.sh

--- site-setup.sh
+++ site-setup.sh
@@ -1,6 +1,11 @@
 sudo mkdir /srv/www
 pushd /srv/www
+
+sudo apt-get install -y git
 sudo git clone https://github.com/ecc521/rivers.run.git
+
+pushd rivers.run
+sudo git checkout express #TODO: Remove
 
 echo "Is the server already set up for multiple sites? If No, setup will be performed. "
 select yn in "Yes" "No"; do
@@ -9,8 +14,6 @@ select yn in "Yes" "No"; do
         Yes ) break;;
     esac
 done
-
-pushd rivers.run
 
 #These two directories are mounted as volumes.
 sudo mkdir server/logs
