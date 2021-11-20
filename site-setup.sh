4a044754b087e2171f5978b36f8a664c013eba97
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-11-19 19:34:38 -0500

    Remove test site specific code

--- site-setup.sh
+++ site-setup.sh
@@ -5,7 +5,6 @@ sudo apt-get install -y git
 sudo git clone https://github.com/ecc521/rivers.run.git
 
 pushd rivers.run
-sudo git checkout express #TODO: Remove
 
 echo "Is the server already set up for multiple sites? If No, setup will be performed. "
 select yn in "Yes" "No"; do
