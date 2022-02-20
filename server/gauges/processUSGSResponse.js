5a6cc22efe4c6c2d7c43bf5bd3482a5cbe5b9e1f
ecc521 <42424091+ecc521@users.noreply.github.com>
2022-02-19 20:44:43 -0500

    Update USGS Parsing Code
    The new code is a bit over twice as fast, and avoids some very rare bugs where misaligned readings could result from holes.

---
+++ server/gauges/processUSGSResponse.js
@@ -0,0 +1,90 @@
+function processUSGSResponse(obj) {
+	//Some of obj.value.queryInfo may be useful for request management (requestDT if using modifiedSince, etc)
+
+	let timeSeries = obj.value.timeSeries
+
+	//USGS sends each data type seperately - we must iterate the list and associate them together.
+	let usgsSites = {}
+
+	for (let i=0;i<timeSeries.length;i++) {
+	    let seriesItem = timeSeries[i]
+	    let siteCode = seriesItem.sourceInfo.siteCode[0].value
+
+	    let obj = usgsSites[siteCode] = usgsSites[siteCode] || {
+	        name: seriesItem.sourceInfo.siteName,
+			readings: new Map()
+	    }
+
+	    let values = seriesItem.values[0].value
+
+	    //Filter out bad readings (usually -999999, caused by things like ice on sensors)
+	    let noDataValue = seriesItem.variable.noDataValue
+	    values = values.filter((value) => {
+	        if (value.value == noDataValue) {return false}
+	        return true
+	    })
+
+		let property;
+
+		//TODO: Also check the codes.
+		// let variableCode = seriesItem.variable.variableCode[0].value
+		//00060 cfs, 00065 feet, 00010 temp (00011 for some like Yough), and 00045 precip
+		let unitCode = seriesItem.variable.unit.unitCode
+		switch (unitCode) {
+			case "deg C":
+				values.forEach((value) => {
+					let tempInF = value.value * 1.8 + 32 //Convert to farenheit
+					value.value = Math.round(tempInF * 100) / 100 //Round to 100ths place.
+				})
+			case "deg F":
+				property = "temp"
+				break;
+			case "ft3/s":
+				property = "cfs"
+				break;
+			case "ft":
+				property = "feet"
+				break;
+			case "in":
+				property = "precip"
+				break;
+			default:
+				console.warn(`Unknown Unit ${unitCode}`)
+				continue; //Continue loop - we can't process this one.
+			}
+
+		values.forEach((value) => {
+			let dateTime = new Date(value.dateTime).getTime()
+
+			let currentReading = obj.readings.get(dateTime)
+			if (!currentReading) {
+				currentReading = {}
+				obj.readings.set(dateTime, currentReading)
+			}
+			currentReading[property] = Number(value.value)
+		})
+	}
+
+	//Now combine the associated data into our format.
+	for (let gaugeID in usgsSites) {
+		let site = usgsSites[gaugeID]
+
+		//Sort timestamps
+		let timestamps = Array.from(site.readings.keys())
+		timestamps.sort() //Ensure the list is sorted.
+
+		let newReadings = []
+
+		timestamps.forEach((timestamp) => {
+			let newReading = site.readings.get(timestamp)
+			newReading.dateTime = timestamp
+			newReadings.push(newReading)
+		})
+
+		site.readings = newReadings
+	}
+
+	return usgsSites
+}
+
+module.exports = processUSGSResponse
--- server/gauges/usgsGauges.js
+++ server/gauges/usgsGauges.js
@@ -7,140 +7,7 @@ const bent = require("bent")
 
 const utils = require(path.join(__dirname, "../", "utils.js"))
 
-function parseUSGS(usgsdata) {
-
-    let usgsarray = {}
-    //Iterate through all known conditions
-    usgsdata.value.timeSeries.forEach(function(event){
-        let obj2 = {}
-        obj2.values = event.values[0].value //The values - ex. Gauge Height Array
-
-        if (obj2.values.length === 0) {
-            return;
-        }
-
-        obj2.units = event.variable.variableDescription //Units of values
-
-        let sitecode = event.sourceInfo.siteCode[0].value
-        //See if the site is already in the array.
-        //If the site code is not in the array, add it.
-        if (!usgsarray[sitecode]) {
-            let obj3 = {}
-            obj3.name = event.sourceInfo.siteName
-            usgsarray[sitecode] = obj3
-        }
-
-        let variablecode = event.variable.variableCode[0].value
-
-        //Convert celcius to farenheight
-        let tempConvert;
-        if (variablecode === "00010" && obj2.units === "Temperature, water, degrees Celsius") {
-            obj2.units = "Temperature, water, degrees Fahrenheit"
-            tempConvert = true
-        }
-
-        let valid = false;
-          for (let i=0;i<obj2.values.length;i++) {
-              //Make sure the value is a number.
-			  	obj2.values[i].value = Number(obj2.values[i].value)
-
-              //If the value is clearly a messed up value (usually because USGS intentially made it absurd to alert of an issue), discard it.
-			  //USGS usually uses -999999 to mark these (I don't think this happens all the time)- however some tidal gauges will go negative for inflow, so
-			  //we can't just check for values below -50, etc.
-				if (obj2.values[i].value === -999999 || isNaN(obj2.values[i].value)) {
-					delete obj2.values[i]
-                    continue;
-				}
-
-            if (tempConvert) {
-                obj2.values[i].value = obj2.values[i].value * 1.8 + 32 //Convert celcius to farenheight
-                obj2.values[i].value = Math.round(obj2.values[i].value*100)/100 ///Make sure we don't get xx.000000000001 or the like by rounding to 100ths place.
-            }
-            delete obj2.values[i].qualifiers //rivers.run doesn't check this. Always seen it saying data is provisional, and nothing else.
-            obj2.values[i].dateTime = new Date(obj2.values[i].dateTime).getTime() //Reformat to decimal based time.
-
-            valid = true
-          }
-
-          if (!valid) {return}
-
-		//Some rivers have code 00011 instead of 00010 (like the Yough).
-		//00011 is farenheit, instead of celcius like 00010, so we don't need to
-		//run any conversions on 00011. Just use instead of 00010.
-		//TODO: Check code. Probably "Temperature, water, degrees Fahrenheit"
-        if (variablecode === "00011") {
-			//console.log("Using code 00011 to fill temperature")
-            variablecode = "00010" //We use 00010 to represent farenhiet.
-        }
-
-        //Add the values onto the site code object
-        usgsarray[sitecode][variablecode] = obj2
-    })
-
-    return usgsarray
-}
-
-
-function reformatUSGS(usgsarray) {
-	for (let gaugeID in usgsarray) {
-		let gauge = usgsarray[gaugeID]
-
-		if (gauge["00060"]) {
-			gauge.cfs = gauge["00060"].values
-			delete gauge["00060"]
-		}
-
-		if (gauge["00065"]) {
-			gauge.feet = gauge["00065"].values
-			delete gauge["00065"]
-		}
-
-		if (gauge["00010"]) {
-			gauge.temp = gauge["00010"].values
-			delete gauge["00010"]
-		}
-
-		if (gauge["00045"]) {
-			gauge.precip = gauge["00045"].values
-			delete gauge["00045"]
-		}
-	}
-	return usgsarray
-}
-
-//Reformat for flowdata3.json
-//This combines the cfs, feet, temp, and precip properties into one array.
-
-function reformatGauge(gauge) {
-	let times = {}
-	let toCombine = ["cfs", "feet", "temp", "precip"]
-	toCombine.forEach((prop) => {
-		if (!gauge[prop]) {return}
-		gauge[prop].forEach((value) => {
-			times[value.dateTime] = times[value.dateTime] || {}
-			times[value.dateTime][prop] = value.value
-		})
-	})
-
-	toCombine.forEach((prop) => {delete gauge[prop]})
-
-	gauge.readings = []
-	for (let prop in times) {
-        let reading = {}
-        reading.dateTime = Number(prop) //Prop appears to be getting converted to a string. Convert it back to a number.
-		Object.assign(reading, times[prop])
-		gauge.readings.push(reading)
-	}
-	return gauge
-}
-
-function reformatGauges(gauges) {
-	for (let id in gauges) {
-		gauges[id] = reformatGauge(gauges[id])
-	}
-	return gauges
-}
-
+const processUSGSResponse = require("./processUSGSResponse.js")
 
 //TODO: Begin using modifiedSince. See https://waterservices.usgs.gov/rest/IV-Service.html
 let getJSON = bent("json", "https://waterservices.usgs.gov/nwis/iv/")
@@ -152,7 +19,7 @@ async function loadSitesFromUSGS(siteCodes, timeInPast = 1000*60*60*24) {
 	let usgsData = await getJSON("?format=json&sites=" + siteCodes.join(",") + period + "&parameterCd=00060,00065,00010,00011,00045&siteStatus=all")
 
     //TODO: We should be able to consolidate these functions.
-	let sites = reformatGauges(reformatUSGS(parseUSGS(usgsData)))
+	let sites = processUSGSResponse(usgsData)
 
     for (let siteCode in sites) {
         sites[siteCode].source = {
