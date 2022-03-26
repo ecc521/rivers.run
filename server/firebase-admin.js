d9bbf43906eb993ada92690a1af5822e42df54e3
ecc521 <42424091+ecc521@users.noreply.github.com>
2022-03-25 23:49:58 -0400

    Add firebase email support
    HAS NOT BEEN TESTED! LIKELY BROKEN!

---
+++ server/firebase-admin.js
@@ -0,0 +1,55 @@
+const path = require("path")
+const utils = require("./utils.js")
+
+const admin = require("firebase-admin");
+
+//TODO: Perhaps use Google Cloud service account lookup or some better method of passing credentials?
+const serviceAccount = path.join(utils.getDataDirectory(), "notifications", "firebase-adminsdk-wzb5p.json")
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+const auth = admin.auth();
+
+const users = db.collection("users")
+
+//TODO: Do listeners count towards reads? Are we double reading here?
+async function loadUserData() {
+	let usersToExamine = await users
+		.where("notifications.enabled", "==", true) //Only where notifications enabled.
+		.where("notifications.noneUntil", "<=", Date.now()) //Only where noneUntil doesn't disable - WARNING: This means noneUntil must have a default value!
+		.get()
+
+	let usersMap = new Map()
+
+	usersToExamine.forEach((queryDocumentSnapshot) => {
+		let data = queryDocumentSnapshot.data()
+		let uid = queryDocumentSnapshot.id
+
+		usersMap.set(uid, data)
+	})
+
+	await loadEmailsForUsers(usersMap)
+
+	let usersArr = Array.from(usersMap.values())
+	console.log(usersArr)
+	return usersArr
+}
+
+async function loadEmailsForUsers(usersMap) {
+	userIDs = Array.from(usersMap.keys())
+
+	userIDs = userIDs.map((userID) => {
+		return {uid: userID}
+	})
+
+	let users = await auth.getUsers(userIDs)
+	users = users.users //There is also a .notFound
+	users.forEach((user) => {
+		usersMap.get(user.uid).auth = user
+	})
+}
+
+module.exports = {loadUserData, loadEmailsForUsers}
