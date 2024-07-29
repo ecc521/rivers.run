9d2319499c7f72b3e69d6e4a993eb5066b059aa7
Tucker Willenborg <tjw47@duke.edu>
2024-07-29 09:48:01 -0400

    Switch app update code to use AppUpdate plugin

--- native/appUpdates.js
+++ native/appUpdates.js
@@ -1,49 +1,43 @@
-//TODO: Dark mode for the update alerts.
+import { AppUpdate, AppUpdateAvailability } from '@capawesome/capacitor-app-update';
 
-module.exports = function() {
-	if (Capacitor?.getPlatform() === "ios") {
-	    try {
-	        ;((async function() {
-	            //Alert for App Updates.
-
-	            //iTunes is settings CORS headers, but those headers don't change when the CDN resends the content -
-	            //if the CDN serves us, the CORS headers are for whatever origin last issued the request.
-
-	            //Fail on Apple's part there. Time to start cache busting. Leave a nice note in case this leaves some weird stuff in logs.
-	            //Apple takes up to a day to stop caching anyways, so this is probably a good thing from that perspective.
-
-	            let req = await fetch("https://itunes.apple.com/lookup?bundleId=run.rivers.twa&YourCDNDoesNotChangeCORSHeadersSoMustCacheBust" + Math.random())
-	            let res = await req.json()
-	            let latestVersion = res.results[0].version
-
-	            let deviceInfo = await window.Capacitor.Plugins.Device.getInfo()
-	            let currentVersion = deviceInfo.appVersion
-
-	            //Using numeric comparison, so version codes can't have more than one decimal.
-	            if (parseFloat(currentVersion) < parseFloat(latestVersion)) {
-					let popup = document.createElement("div")
-					popup.innerHTML = `
-					<h2>App Update</h2>
-					<h3>There is a Rivers.run <a href='https://apps.apple.com/us/app/rivers-run/id1552809249' target='_blank'>app update</a>. Downloading it is recommended. You may experience issues if you do not update.</h3>`
-					popup.style.left = popup.style.top = popup.style.bottom = popup.style.right = "0"
-					popup.style.position = "absolute"
-					popup.style.textAlign = "center"
-					popup.style.backgroundColor = "white"
-					popup.style.color = "black"
-					popup.style.padding = "10px"
-					popup.style.paddingTop = "30px"
-					let closeButton = document.createElement("button")
-					closeButton.innerHTML = "Close"
-					closeButton.style.padding = "20px"
-					closeButton.addEventListener("click", function() {
-						popup.remove()
-					})
-
-					popup.appendChild(closeButton)
-					document.body.appendChild(popup)
-	            }
-	        })())
-	    }
-	    catch (e) {console.error(e)}
+async function createPopupIfUpdate() {
+	let appUpdateInfo = await AppUpdate.getAppUpdateInfo();
+	if (appUpdateInfo.updateAvailability !== AppUpdateAvailability.UPDATE_AVAILABLE) {
+		return;
 	}
+
+	let popup = document.createElement("div")
+	popup.innerHTML = `
+					<h1>App Update</h1>
+					<h2>There is a Rivers.run app update. Downloading it is recommended. You may experience issues if you do not update.</h2>`
+	popup.style.left = popup.style.top = popup.style.bottom = popup.style.right = "0"
+	popup.style.position = "absolute"
+	popup.style.textAlign = "center"
+	popup.style.backgroundColor = "white"
+	popup.style.color = "black"
+	popup.style.padding = "10px"
+	popup.style.paddingTop = "30px"
+
+	let beginUpdateButton = document.createElement("button")
+	beginUpdateButton.innerHTML = "Update Now"
+	beginUpdateButton.style.padding = "20px"
+	beginUpdateButton.style.fontSize = "2em"
+	beginUpdateButton.addEventListener("click", function() {
+		AppUpdate.openAppStore()
+	})
+	popup.append(beginUpdateButton)
+
+	let closeButton = document.createElement("button")
+	closeButton.innerHTML = "Close"
+	closeButton.style.padding = "20px"
+	closeButton.style.fontSize = "2em"
+	closeButton.addEventListener("click", function() {
+		popup.remove()
+	})
+
+	popup.appendChild(closeButton)
+	document.body.appendChild(popup)
 }
+
+
+export {createPopupIfUpdate}
